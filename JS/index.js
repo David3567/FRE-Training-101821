65f2fb055954a0ee0baff6b7773fc58de1d540b5
Patrick-FRE <patrick.lin@antra.com>
2021-10-21 16:51:20 -0400

    update JS day one

---
+++ JS/index.js
@@ -0,0 +1,66 @@
+// NODE, Javascript, EcmaScript(ES5, ES6, ES7...)
+// EcmaScript = Sytanx standard
+// Javacirpt =  EcmaScript + webAPI
+// NODE = EcmaScript + NODEAPI
+
+// primative type 
+// let num =5; // number
+// console.log(typeof num)
+// let str = 'abc';
+// console.log(typeof str)
+// let b = true;
+// console.log(typeof b)
+// let und = undefined;
+// console.log(typeof und)
+// let n = null;
+// console.log(typeof n);
+
+// let c = 10;
+// let d = c;
+// d = 5;
+// console.log('d',d) // 5
+// foo(c);
+
+// console.log("c",c) // 10
+
+// function foo(input){
+//     input = 7;
+//     console.log("input",input) // 7
+// }
+
+/// object data type : object | function
+// function foo(){
+
+// }
+// console.log(typeof foo);
+// let obj = {}
+// console.log(typeof obj);
+// let arr = []
+// console.log(typeof arr);
+
+let obj = {name:'patrick'}
+
+foo(obj);
+console.log(obj)
+function foo(input){
+    input.name = 'changed'
+    console.log(input)
+}
+
+// deepcopy, shallow copy
+// class key in ES6 and constructor function in ES5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
