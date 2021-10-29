1906a4de769d60e6ab0208c69302cf979afc0d16
Patrick-FRE <patrick.lin@antra.com>
2021-10-29 13:12:57 -0400

    Update Promise

--- JS/index.js
+++ JS/index.js
@@ -104,7 +104,6 @@
 //   console.log(`I am ${this.pname}`);
 // };
 
-
 // function Employee(name, age, company) {
 //   Person.prototype.constructor.call(this, name, age); // super in ES6
 //   this.company = company;
@@ -163,9 +162,8 @@
 
 // foo(undefined,undefined,"hello",()=>{});
 
-let arr = [1,2,3]; // [2 ,4 ,6]
-let arr2 = [2,4,6] // [4,8,12]
-
+// let arr = [1, 2, 3]; // [2 ,4 ,6]
+// let arr2 = [2, 4, 6]; // [4,8,12]
 
 // Array.prototype.filter
 // Array.prototype.map
@@ -173,20 +171,237 @@ let arr2 = [2,4,6] // [4,8,12]
 // Array.prototype.some
 // Array.prototype.every
 
+// Array.prototype.myforEach = function (callbackFn) {
+//   for (let i = 0; i < this.length; i++) {
+//     callbackFn(this[i], i, this);
+//   }
+// };
 
+// function multiplyByTwo(arr) {
+//   let res = [];
+//   arr.myforEach((item, index, array) => {
+//     console.log('item', item, 'index', index, 'array', array);
+//     res.push(item * 2);
+//   });
+//   return res;
+// }
+// console.log(multiplyByTwo(arr2));
 
-Array.prototype.myforEach = function(  callbackFn ){
-  for(let i = 0; i<this.length;i++){
-    callbackFn(this[i],i,this)
-  }
+// const arr = [1,2,3];
+
+// const arr2 = arr.map(item=>item*2);
+// const arr22 = arr.reduce((acc,cur,index,array)=>{
+//   console.log(acc,cur,"index:",index, array)
+//    //  acc.push(cur*2);
+//    return acc;
+// },[])
+
+// //console.log(arr22,arr2)
+
+// //console.log(arr2);
+// const arr3 = arr.filter(item=>{
+//   return item % 2 ===0
+// });
+// //console.log(arr3);
+
+// const arr5 = arr.reduce((acc, cur, index, array)=>{
+//   acc = acc + cur
+//   return acc
+// },0)
+
+// console.log(arr5)
+
+// ES6
+// arrow function
+// hoisting
+// let const var
+//console.log(foo)
+
+// function foo(){
+//   console.log('foo')
+// }
+// console.log(foo2) ///
+// foo2()
+// var foo2 = function(){
+//   console.log('foo2')
+// }
+// //foo2()
+// var foo3 = ()=>{
+//   console.log('foo3')
+// }
+// //foo3()
+
+// console.log(a);
+// var a = 5;
+
+// let a ;
+// a =6;
+// console.log(a)
+// const b;
+// b = 6;
+// console.log(b)
+
+// const obj = {name:'patrick'};
+// obj.name = 5;
+// obj.age = 12
+// console.log(obj)
+
+// function foo(){
+//  // console.log(a)// undefined
+//   if(true){
+//     let a = 5;
+//     console.log(a)// 5
+//   }
+//   //console.log(a)// 5
+// }
+
+// foo()
+// console.log(a) // 5
+
+// function foo(){
+//   for(var i = 0;i<5;i++){
+//     setTimeout(()=>{
+//       console.log(i);
+//     },i*1000)
+//   }
+// }
+// foo()
+
+// function foo(){
+
+//   for(let i = 0; i<20;i++){
+//     // if(i === 0){
+//     //   setTimeout(()=>{
+//     //       console.log(i)
+//     //   },0)
+//     // }
+//   }
+// }
+// foo()
+
+// function dosomethingOne(){
+//   console.log('dosomethingOne')
+// }
+
+// function dosomethingTwo(){
+//   console.log('dosomethingTwo')
+// }
+function dosomething(msg) {
+  console.log(`doing thing ${msg}`);
 }
 
-function multiplyByTwo(arr){
-  let res = [];
-  arr.myforEach((item,index,array)=>{
-    console.log('item',item, 'index',index,"array",array)
-    res.push(item*2);
+function callAfter2SecondWithPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('next');
+    }, 2000);
+  });
+}
+
+callAfter2SecondWithPromise()
+  .then((_) => {
+    dosomething(1);
+    return callAfter2SecondWithPromise();
   })
-  return res;
-}
-console.log(multiplyByTwo(arr2))
+  .then((_) => {
+    dosomething(2);
+    return callAfter2SecondWithPromise();
+  })
+  .then((_) => {
+    dosomething(3);
+    return callAfter2SecondWithPromise();
+  })
+  .then((_) => {
+    dosomething(4);
+    return callAfter2SecondWithPromise();
+  })
+  .then((_) => {
+    dosomething(5);
+    return callAfter2SecondWithPromise();
+  });
+
+// function callAfter2Second(callbackFn){
+//   setTimeout(()=>{
+//     callbackFn()
+//   },2000)
+// }
+
+// callAfter2Second(()=>{
+//   dosomething(1);
+//   callAfter2Second(()=>{
+//     dosomething(2);
+//     callAfter2Second(()=>{
+//       dosomething(3);
+//       callAfter2Second(()=>{
+//         dosomething(4);
+//         callAfter2Second(()=>dosomething(5));
+//       });
+//     });
+//   });
+// });
+
+// callAfter2Second(dosomethingOne);
+// callAfter2Second(dosomethingTwo);
+
+// class MyPromise {
+//   constructor(cb) {
+//     this.resolve = this.resolve.bind(this);
+//     this.PromiseState = 'pending';
+//     this.onFufillmentList = [];
+//     cb(this.resolve);
+//   }
+
+//   resolve(value) {
+//     this.PromiseState = 'fufill';
+//     this.value = value;
+//     setTimeout(async() => {
+//       for(let i = 0; i<this.onFufillmentList.length;i++){
+//         const result = await this.onFufillmentList[i](this.value);
+//         this.value = result;
+//       }
+//     });
+//   }
+
+//   then(onFufillmentFn) {
+//     this.onFufillmentList.push(onFufillmentFn);
+//     return this;
+//   }
+// }
+
+// console.log('before new Prmomise');
+// let p = new MyPromise((resolve, reject) => {
+//   console.log('hello');
+//   setTimeout(() => {
+//     resolve({ name: 'patrick' });
+//   }, 3000);
+// });
+// p.then((value) => {
+//   console.log('value', value);
+//   // return new Promise((res, rej) => {
+//   //   res("hello");
+//   // });
+
+//   return "no Promise any more"
+// }).then((value2) => {
+//   console.log("value2",value2);
+// });
+// console.log('After new Prmomise');
+// console.log(p);
+
+// console.log('================');
+
+// console.log(1)
+// new Promise((resolve,reject)=>{
+//   console.log(2);
+//   resolve(3)
+// }).then(data=>{
+//   console.log(data)
+// })
+// console.log(4)
+
+//p.then(console.log(5))
+
+// console.log(p);
+// setTimeout(()=>{
+//   console.log(p);
+// },4000)
