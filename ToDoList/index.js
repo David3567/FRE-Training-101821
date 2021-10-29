4b002403de559ff56cea31886f43965208180707
yakobmk <yakobmk98@gmail.com>
2021-10-29 10:27:21 -0400

    finished todo list

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -1,39 +1,71 @@
-let value = document.getElementById('one');
-// value.innerHTML = "hello"
-console.log(value);
+// To do list using HTML, CSS & JS only
 
-// addind delete buttons to all tasks listed
-let myList = document.getElementsByTagName("li");
-for (let x = 0; x<myList.length; x++){
-    let spanItem = document.createElement("span");
-    let closeIcon = document.createTextNode("\u00D7");
-    spanItem.className = "close";
-    spanItem.appendChild(closeIcon);
-    myList[x].appendChild(spanItem);
-    console.log("first one", myList[x]);
+function addDelBtn(){
+    let myList = document.getElementsByTagName("li");
+    for (let x = 0; x<myList.length; x++){
+        let spanItem = document.createElement("span");
+        let closeIcon = document.createTextNode("\u00D7");
+        spanItem.className = "close";
+        spanItem.appendChild(closeIcon);
+        myList[x].appendChild(spanItem);
+        // console.log("first one", myList[x]);
 
+    }
+    return;
 }
+
+addDelBtn()
+
 
 
 // Need to loop over the already provided list
-myApproved = document.getElementsByClassName("close");
-// console.log("myApproved", myApproved)
-for (var i = 0; i < myApproved.length; i++) {
-    console.log("current", myApproved[i])
-    myApproved[i].onclick = function () {
-        var val = this.parentElement;
-        console.log("parent element", val)
-        val.style.display = "none";
+
+function removeItem () {
+    myApproved = document.getElementsByClassName("close");
+    for (var i = 0; i < myApproved.length; i++) {
+        console.log("current", myApproved[i])
+        myApproved[i].onclick = function () {
+            var val = this.parentElement;
+            console.log("parent element", val)
+            val.style.display = "none";
+        }
     }
+    return;
+}
+
+removeItem()
+
+
+// Executing the add button function
+// get the event being typed {check error that a string is put in}
+// at the point that it becomes successful then list it as the child of the the ul
+
+function myAddFn() {
+    var newElement = document.createElement("li");
+    var newTask = document.getElementById("userInput").value;
+    var spanItem = document.createTextNode(newTask);
+    newElement.appendChild(spanItem);
+
+
+    if (newTask == ''){
+        alert ("You Must Write Something")
+    } else {
+        document.getElementById("listedItems").appendChild(newElement)
+    }
+    document.getElementById("userInput").value = '';
+
+
+    // Adding Del button specific to the newly added item
+    var close = document.createElement("span");
+    var closeIcon = document.createTextNode("\u00D7");
+    close.className = "close";
+    close.appendChild(closeIcon);
+    newElement.appendChild(close);
+
+
+    removeItem();
+    return;
+
 }
 
 
-
-
-// console.log("total Items", myList.length);
-// console.log("myList", myList);
-
-
-// function myFunction() {
-//     document.getElementsById("one").innerHTML = "changed"
-// }
