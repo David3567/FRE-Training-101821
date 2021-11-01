e59165bc5ed1d9b50e0770d41659df68bb56e110
lh2510 <lh2510@nyu.edu>
2021-11-01 01:46:36 -0400

    update

--- practice/todolist4/js/index.js
+++ practice/todolist4/js/index.js
@@ -10,17 +10,24 @@ const Model = (() => {
 			headers: {
 				'Content-type': 'application/json; charset=UTF-8',
 			},
-		}).then((response) => response.json()) //return a promise
+		}).then((response) => {
+			console.log(response.ok)
+			if (!response.ok) {
+				throw new Error('HTTP status ' + response.status)
+			}
+			return response.json()
+		}) //return a promise
 	}
 
 	const deleteTodo = (id) => {
 		return fetch(`http://localhost:8000/api/todo/${id}/delete`, {
 			method: 'DELETE',
 		}).then((response) => {
+			console.log(response.ok)
 			if (!response.ok) {
 				throw new Error('HTTP status ' + response.status)
 			}
-			response.json()
+			return response.json()
 		}) //return a promise
 	}
 	return { fetchTodos, addTodo, deleteTodo } //same as { fetchTodos: fetchTodos }
@@ -40,7 +47,7 @@ const View = (() => {
 
 	const generateTodoItem = (todo) => {
 		const title = todo.task.length > 30 ? todo.task.substring(0, 30) + '...' : todo.task
-		return `<li id="${todo._id}" class="todolist__row todolist__content-item"><span class="todolist__content-item-title">${title}</span> <button class="btn btn-delete">Delete</button></li>`
+		return `<li id="${todo._id}" class="todolist__row todolist__content-item"><span class="todolist__content-item-title">${title}</span> <button class="btn btn-edit">Edit</button> <button class="btn btn-delete">Delete</button></li>`
 	}
 	const initTodoListTemplate = () => {
 		return `<section class='todolist'>
@@ -59,7 +66,7 @@ const View = (() => {
 })()
 
 const AppController = ((model, view) => {
-	let todolistdata
+	//let todolistdata
 	const init = () => {
 		const initTemplate = view.initTodoListTemplate()
 		const initElement = document.querySelector(view.domString.todoListContainer)
@@ -68,7 +75,7 @@ const AppController = ((model, view) => {
 		setUpAddEvent()
 	}
 
-	const updateTodoListItems = (data) => {
+	const updateTodoListItems_initialLoad = (data) => {
 		let todoListTemple = data
 			.map((todo) => {
 				return view.generateTodoItem(todo)
@@ -77,35 +84,18 @@ const AppController = ((model, view) => {
 		//console.log('todoListTemple,', todoListTemple)
 		const todoListElement = document.querySelector(view.domString.todoListContent)
 		view.render(todoListTemple, todoListElement)
-		setUpDeleteEvent() //after we render todolist items, then we setup delete event
+		setUpDeleteEvent_initialLoad() //after we render todolist items, then we setup delete event
 	}
 
 	const fetchData = () => {
 		model.fetchTodos().then((data) => {
 			//model.fetchTodos() return promise
 			console.log('data:', data) //array
-			todolistdata = data
-			updateTodoListItems(todolistdata)
-		})
-	}
-	const setUpAddEvent = () => {
-		//for add
-		const btnAdd = document.querySelector(view.domString.todoListAdd)
-		const todolistInput = document.querySelector(view.domString.todoListInput)
-		btnAdd.addEventListener('click', () => {
-			model
-				.addTodo({
-					task: todolistInput.value,
-				})
-				.then((data) => {
-					console.log('data:', data)
-					todolistdata.unshift(data.task)
-					updateTodoListItems(todolistdata)
-				})
+			updateTodoListItems_initialLoad(data)
 		})
 	}
 
-	const setUpDeleteEvent = () => {
+	const setUpDeleteEvent_initialLoad = () => {
 		const btnDeleteList = document.querySelectorAll(view.domString.todoListDelete)
 
 		btnDeleteList.forEach((btnDelete) => {
@@ -121,6 +111,48 @@ const AppController = ((model, view) => {
 						console.log('error occurred:', err)
 					})
 			})
+		})
+	}
+
+	const setUpAddEvent = () => {
+		//for add
+		const btnAdd = document.querySelector(view.domString.todoListAdd)
+		const todolistInput = document.querySelector(view.domString.todoListInput)
+		btnAdd.addEventListener('click', () => {
+			model
+				.addTodo({
+					task: todolistInput.value,
+				})
+				.then((data) => {
+					console.log('data:', data)
+					// todolistdata.unshift(data.task)
+					// updateTodoListItems(todolistdata)
+					let element = document.createElement('li')
+					element.setAttribute('id', data.task._id)
+					element.classList.add('todolist__row')
+					element.classList.add('todolist__content-item')
+					element.innerHTML = `<span class='todolist__content-item-title'>${data.task.task}</span> <button class="btn btn-edit">Edit</button> <button class='btn btn-delete'>Delete</button>`
+					let parentElement = document.querySelector('.todolist__content')
+					parentElement.insertBefore(element, parentElement.firstChild)
+
+					//setup delete event listener for this new item
+					let button = element.querySelector('button')
+					button.addEventListener('click', () => {
+						console.log(button.parentElement.id)
+						model
+							.deleteTodo(button.parentElement.id)
+							.then((data) => {
+								console.log('data:', data)
+								document.getElementById(button.parentElement.id).style.display = 'none'
+							})
+							.catch((err) => {
+								console.log('error occurred:', err)
+							})
+					})
+				})
+				.catch((err) => {
+					console.log('error occurred:', err)
+				})
 		})
 	}
 
