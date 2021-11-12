c01bfd180e3d6ed6eb5058962328b9d629cf8942
hmao1 <35634135+hmao1@users.noreply.github.com>
2021-11-12 12:07:14 -0500

    add babel

---
+++ reactApp/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import './styles/styles.css';
+
+
+ReactDOM.render(
+   <h1>test babel</h1>,
+    document.querySelector('#fromjs')
+);
