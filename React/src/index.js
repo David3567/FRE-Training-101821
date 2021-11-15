a12a40265e9087072c0f856925ffb66d1687b1c2
Patrick-FRE <patrick.lin@antra.com>
2021-11-15 17:12:17 -0500

    update React boilerplate

--- React/src/index.js
+++ React/src/index.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './components/Button';
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+      <>
+        <div>Hello!!!!! {this.props.name}</div>
+        <Button />
+      </>
+    );
   }
 }
 
 ReactDOM.render(
-  <HelloMessage name="Taylor" />,
+  <HelloMessage name="Patrick" />,
   document.getElementById('hello-example')
 );
+
+const patrick = { name: 'patrick' };
