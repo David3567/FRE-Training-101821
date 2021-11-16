fcf0c5d115d587857e849529395ddecbb81bb96b
michaeldegennaro <michaeldegennaro58@yahoo.com>
2021-11-16 13:33:24 -0500

    finished homework

--- create-react/new-react-app/src/index.js
+++ create-react/new-react-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';   
+import './style.scss';
+
 
 ReactDOM.render(
     <h1>Hello from index.js</h1>,
--- create-react/new-react-app/webpack.config.js
+++ create-react/new-react-app/webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     mode: 'development',
     module: {
         rules: [
-          {
+          { 
             test: /\.m?js$/,
             exclude: /node_modules/,
             use: {
@@ -14,6 +14,14 @@ module.exports = {
               },
             },
           },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              "style-loader",
+              "css-loader",
+              "sass-loader",
+          ],
+      },
         ],
       },
     devServer: {
