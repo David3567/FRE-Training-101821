d04810c6083bb498cee2de9c77fc82e65581bdcc
Brian Yao <byao321@gmail.com>
2021-11-16 12:13:02 -0500

    Added scss to webpack

---
+++ Webpack_Test/src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+	return <button>My Button</button>;
+};
+
+export default Button;
---
+++ Webpack_Test/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	entry: "./src/index.js",
+	mode: "production",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.(scss|sass|css)$/,
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+							sourceMap: true,
+							importLoaders: 1,
+						},
+					},
+					"sass-loader",
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "public", "index.html"),
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+			ignoreOrder: false,
+		}),
+	],
+};
