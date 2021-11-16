6cd6c387ac5354651f921ad5126d401e79a6276c
Benson Tsang <df981339@gmail.com>
2021-11-16 13:37:25 -0500

    Set up scss

--- React/src/index.js
+++ React/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles/styles.scss";
 
 class App extends React.Component {
   render() {
--- React/webpack.config.js
+++ React/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     rules: [
       { test: /\.(js|jsx)$/, use: "babel-loader" },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
     ],
   },
 
