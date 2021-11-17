00b3dac3f151aa32461f3f01bccbb6ecb998b471
David An <taean2084@gmail.com>
2021-11-17 12:40:26 -0500

    my first boilerplate

---
+++ Advanced/my-boilerplate/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/style.scss';
+
+ReactDOM.render(
+    <h1>Hello React World</h1>,
+  document.getElementById('root')
+);
---
+++ Advanced/my-boilerplate/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { devServer } = require('../webpack-project/webpack.config');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.js'),
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.css', 'scss', 'sass'],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: './bundle.js'
+    },
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new DuplicatePackageCheckerPlugin(),
+        new MiniCssExtractPlugin(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+        ]
+    }
+};
