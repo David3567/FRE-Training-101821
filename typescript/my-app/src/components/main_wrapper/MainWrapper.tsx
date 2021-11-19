9b20d152b0ffb85cbed0b46d0eed17f3409ee616
lh2510 <lh2510@nyu.edu>
2021-11-19 14:12:03 -0500

    update

--- typescript/my-app/src/components/main_wrapper/MainWrapper.tsx
+++ typescript/my-app/src/components/main_wrapper/MainWrapper.tsx
@@ -16,14 +16,16 @@ interface Props {
 export default class MainWrapper extends Component<Props, State> {
 	constructor(props: Props) {
 		super(props)
-		this.state = { name: this.props.name, albumArr: [], albumCount: -1, offset: 1 }
+		this.state = { name: this.props.name, albumArr: [], albumCount: 0, offset: 1 }
 	}
 
 	handleClick = () => {
-		this.setState({ offset: this.state.offset + 10 })
 		fetchTenAlbumList(this.props.name, this.state.offset + 10).then((data) => {
 			if (data) {
+				this.setState({ offset: this.state.offset + 10 })
 				this.setState({ albumArr: this.state.albumArr.concat(data.results), albumCount: this.state.albumCount + data.resultCount })
+			} else {
+				console.log('error')
 			}
 		})
 	}
@@ -31,7 +33,7 @@ export default class MainWrapper extends Component<Props, State> {
 	componentDidUpdate(prevProps: Props) {
 		// Typical usage (don't forget to compare props):
 		if (this.props.name !== prevProps.name) {
-			this.setState({ name: this.props.name, albumArr: [], albumCount: -1, offset: 1 })
+			this.setState({ name: this.props.name, albumArr: [], albumCount: 0, offset: 1 })
 			fetchTenAlbumList(this.props.name, 1).then((data) => {
 				console.log('data', data)
 				if (data) {
@@ -45,7 +47,7 @@ export default class MainWrapper extends Component<Props, State> {
 		return (
 			<main className='app__main'>
 				<div className='app__counter'>
-					<p>{this.state.albumCount !== -1 && this.state.name !== '' ? `${this.state.albumCount} results for "${this.state.name}"` : 'Search Albums by ArtistName:'}</p>
+					<p>{this.state.name !== '' ? `${this.state.albumCount} results for "${this.state.name}"` : 'Search Albums by ArtistName:'}</p>
 				</div>
 				<div className='card-container'>
 					{this.state.albumArr.map((item, index) => {
@@ -57,7 +59,7 @@ export default class MainWrapper extends Component<Props, State> {
 						className='a-next a-round'
 						onClick={this.handleClick}
 						style={{
-							display: this.state.albumCount !== -1 && this.state.name !== '' && this.state.albumCount !== 0 ? 'inline' : 'none',
+							display: this.state.albumCount !== 0 && this.state.name !== '' && this.state.albumCount !== 0 ? 'inline' : 'none',
 						}}
 					>
 						load more
