8cb0d6b11a47e374815887bfd67db6e7d3df1383
David An <taean2084@gmail.com>
2021-11-22 00:51:37 -0500

    update

--- event-app/src/App.js
+++ event-app/src/App.js
@@ -1,32 +1,11 @@
-import { useState } from 'react';
 import './App.css';
-import $ from 'jquery'
+import Counter from './Counter';
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
-  const click = () => {
-    console.log('clicked');
-    setCounter(counter => counter + 1);
-  }
-
-  const hide = () => {
-    $('.addBtn').toggle();
-  }
-
-  const alarm = () => {
-    setTimeout (() => {
-      alert(counter)
-    }, 5000)
-  }
+  
 
   return (
-    <div className="App">
-      <div>{counter}</div>
-      <button className="addBtn" onClick={click}>Add</button>
-      <button classNAme="hideAddBtn" onClick={hide}>Hide Add Button</button>
-      <button className="alertBtn" onClick={alarm}>Alert</button>
-    </div>
+    <Counter />
   );
 }
 
---
+++ event-app/src/Counter.js
@@ -0,0 +1,32 @@
+import React, {useState } from 'react'
+import $ from 'jquery';
+
+const Counter = () => {
+
+    let [counter, setCounter] = useState(0);
+
+    const click = () => {
+        console.log('clicked');
+        setCounter(counter => counter + 1);
+    }
+
+    const hide = () => {
+        $('.addBtn').toggle();
+    }
+
+    const alarm = () => {
+        setTimeout (() => {
+        alert(counter)
+        }, 5000)
+    }
+    return (
+    <div className="App">
+        <div>{counter}</div>
+        <button className="addBtn" onClick={click}>Add</button>
+        <button classNAme="hideAddBtn" onClick={hide}>Hide Add Button</button>
+        <button className="alertBtn" onClick={alarm}>Alert</button>
+    </div>
+    )
+}
+
+export default Counter;
