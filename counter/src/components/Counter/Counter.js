2f48b0db263d804520c889a967e0ffe7d3a2d031
blackenjoy27 <lixunxin@gmail.com>
2021-11-21 23:38:43 -0500

    Finish HW

---
+++ counter/src/components/Counter/Counter.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import Button from '../Button/Button';
+
+let data = {
+  counter: 1000,
+};
+// UI = React (state)
+
+const Counter = (props) => {
+  const [appInfo, setAppInfo] = useState({
+    title: 'counter',
+    counter: 0,
+    hideBtnAdd: false,
+    keep: false,
+  })
+
+  const [keep, setKeep] = useState(false);
+
+  useEffect(() => {
+    console.log('Counter componentDidMount');
+  }, [])
+
+  useEffect(() => {
+    const temp = appInfo.counter;
+    setTimeout(() => {
+      alert(temp); // HW2 alert the recent value using function component
+    }, 5000);
+  }, [keep])
+
+
+
+
+  return (
+    <section>
+      {props.children}
+      <header>Counter:{appInfo.counter}</header>
+
+      {!appInfo.hideBtnAdd ? (
+        <Button
+          className="btn"
+          onClick={() => {
+            //HW1  why
+            // Object.assign() is trigger, last one win setState will be implement
+            setAppInfo((preState) => ({ counter: preState.counter + 1 }));
+            setAppInfo((preState) => ({ counter: preState.counter + 1 }));
+            setAppInfo((preState) => ({ counter: preState.counter + 1 }));
+          }}
+        >
+          <h1>ADD</h1>
+        </Button>
+      ) : null}
+
+      <Button
+        onClick={() => {
+          setAppInfo({ ...appInfo, hideBtnAdd: !appInfo.hideBtnAdd });
+        }}
+      >
+        Remove/Show Btn Add
+      </Button>
+      <Button
+        onClick={() => {
+          setKeep(!keep);
+        }}
+      >
+        Alert After 5s
+      </Button>
+    </section>
+  );
+}
+
+export default Counter;
