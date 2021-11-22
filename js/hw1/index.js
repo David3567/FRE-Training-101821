cf8a4af52bc2d4fb34cced89e6089ac1b83061bd
Earl Perez <ebcp.dev@gmail.com>
2021-11-21 22:14:46 -0800

    modified hw

--- js/hw1/index.js
+++ js/hw1/index.js
@@ -3,7 +3,7 @@ const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log('Nums: ', nums);
 
 // Array.prototype.filter
-Array.prototype.myFilter = function(callbackFn, thisArg) {
+Array.prototype.myFilter = function (callbackFn, thisArg) {
   const filtered = [];
   for (let i = 0; i < this.length; i++) {
     if (callbackFn(this[i], i, this)) {
@@ -11,10 +11,9 @@ Array.prototype.myFilter = function(callbackFn, thisArg) {
     }
   }
   return filtered;
-}
+};
 const odds = nums.filter((num) => num % 2 !== 0);
 console.log('myFilter: ', odds);
-
 
 // Array.prototype.map
 Array.prototype.myMap = function (callbackFn, thisArg) {
@@ -23,8 +22,8 @@ Array.prototype.myMap = function (callbackFn, thisArg) {
     newArr.push(callbackFn(this[i], i, this));
   }
   return newArr;
-}
-const timesTen = nums.myMap(num => num * 10);
+};
+const timesTen = nums.myMap((num) => num * 10);
 console.log('myMap: ', timesTen);
 
 // Array.prototype.reduce
@@ -35,17 +34,20 @@ Array.prototype.myReduce = function (callbackFn, initialValue) {
     arr = arr.slice(1);
   }
   let result = initialValue;
-  arr.forEach(el => result = callbackFn(result, el));
+  arr.forEach((el) => (result = callbackFn(result, el)));
 
   return result;
-}
+};
 const sum = nums.myReduce((accumulator, current) => accumulator + current);
-const sumWithInitial = nums.myReduce((accumulator, current) => accumulator + current, 5);
+const sumWithInitial = nums.myReduce(
+  (accumulator, current) => accumulator + current,
+  5
+);
 console.log('myReduce: ', sum); //55
 console.log('myReduceWithInitial: ', sumWithInitial); // 60
 
 // Array.prototype.some
-Array.prototype.mySome = function(callbackFn, thisArg) {
+Array.prototype.mySome = function (callbackFn, thisArg) {
   let result = false;
   for (let i = 0; i < this.length; i++) {
     if (callbackFn(this[i], i, this)) {
@@ -54,24 +56,24 @@ Array.prototype.mySome = function(callbackFn, thisArg) {
     }
   }
   return result;
-}
-const greaterThan10 = nums.mySome(num => num > 10); // False
-const lessThan10 = nums.mySome(num => num < 10); // True
+};
+const greaterThan10 = nums.mySome((num) => num > 10); // False
+const lessThan10 = nums.mySome((num) => num < 10); // True
 console.log('mySome>10', greaterThan10);
 console.log('mySome<10', lessThan10);
 
 // Array.prototype.every
-Array.prototype.myEvery = function(callbackFn, thisArg) {
+Array.prototype.myEvery = function (callbackFn, thisArg) {
   let result = true;
   for (let i = 0; i < this.length; i++) {
     if (!callbackFn(this[i], i, this)) {
       result = false;
       break;
-    } 
+    }
   }
   return result;
-}
-const greaterThan0 = nums.myEvery(num => num > 0); // True
-const lessThan9 = nums.myEvery(num => num < 9); // False
+};
+const greaterThan0 = nums.myEvery((num) => num > 0); // True
+const lessThan9 = nums.myEvery((num) => num < 9); // False
 console.log('myEvery>0', greaterThan0);
 console.log('myEvery<9', lessThan9);
