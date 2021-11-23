dbb33b01c3cd43b961fc7596836231d48910f1f3
Patrick-FRE <patrick.lin@antra.com>
2021-11-23 17:27:55 -0500

    Update HOC

--- even-app/src/index.js
+++ even-app/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import StockApp from './components/StockApp/StockApp';
+
+ReactDOM.render(<StockApp />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
