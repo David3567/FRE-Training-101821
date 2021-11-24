031067b806e147d178f7b09abceb6c276db8e70a
Waynexia888 <qiwuwaynexia@gmail.com>
2021-11-24 08:05:53 -0800

    withBuyStock.js

---
+++ even-app/src/hoc/withBuyStock.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export const withBuyStockData = (WrappedComponent) => {
+    return () => {
+        const [stockNum, setStockNum] = useState(0);
+
+        const handleAdd = () => {
+            setStockNum(stockNum + 1);
+        };
+        const handleSub = () => {
+            setStockNum(stockNum - 1);
+        };
+
+        return (
+            <WrappedComponent
+                stockNum={stockNum}
+                handleAdd={handleAdd}
+                handleSub={handleSub}
+            ></WrappedComponent>
+        );
+    }
+};
