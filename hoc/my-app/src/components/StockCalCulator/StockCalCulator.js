9d0940c4f1d7779011bedccd63ed4f1c4e022d95
lh2510 <lh2510@nyu.edu>
2021-11-23 20:44:31 -0500

    init

---
+++ hoc/my-app/src/components/StockCalCulator/StockCalCulator.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { withBuyStockData } from '../hoc/withBuyStock'
+//MVC vs  MVVM
+//Data binding:
+//from model to view : attrbute Or children JSX binding
+//from view to model : event binding
+//Controlled Component | UnControlled Component
+
+class StockCalCulator extends React.Component {
+	state = {
+		currentStock: 11,
+	}
+
+	render() {
+		return (
+			<section>
+				<header>StockCalCulator</header>
+				<h1
+					onClick={(e) => {
+						e.target.innerHTML = 'Changed'
+						e.target.style.color = 'red'
+					}}
+				>
+					How Many Stock do you have?
+				</h1>
+				<input type='number' onChange={(e) => this.setState({ currentStock: +e.target.value })} value={this.state.currentStock} />
+
+				<h1>
+					How Many Stock you want to buy
+					<button onClick={this.props.handleAdd}>+</button>
+					<span>{this.props.stockNum}</span>
+					<button onClick={this.props.handleSub}> -</button>
+				</h1>
+				<p>You have {this.state.currentStock + this.props.stockNum} stock </p>
+			</section>
+		)
+	}
+}
+
+const StockCalCulatorContainer = withBuyStockData(StockCalCulator)
+
+export default StockCalCulatorContainer
