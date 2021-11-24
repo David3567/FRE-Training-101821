a2c1bde474c6ae5bd464ee05b0a5c5080b9321f9
David An <taean2084@gmail.com>
2021-11-24 15:04:38 -0500

    update hoc

---
+++ event-app/src/components/StockApp/StockApp.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import BuyStock from './BuyStock/BuyStock';
+import StockCalCulator from './StockCalCulator/StockCalCulator';
+
+class StockApp extends React.Component {
+  state = {
+    BuyStock: false,
+    StockCalCulator: false,
+  };
+
+  hanldeBuyStockPage = (e) => {
+    e.preventDefault();
+    this.setState({ BuyStock: true, StockCalCulator: false });
+  };
+  hanldeStockCalCulatorPage = (e) => {
+    e.preventDefault();
+
+    this.setState({ BuyStock: false, StockCalCulator: true });
+  };
+
+  render() {
+    return (
+      <div>
+        <header>
+          <a
+            href="#"
+            onClick={this.hanldeBuyStockPage}
+            style={{ marginRight: '20px' }}
+          >
+            BuyStock
+          </a>
+          <a href="#" onClick={this.hanldeStockCalCulatorPage}>
+            StockCalCulator
+          </a>
+        </header>
+        {this.state.BuyStock ? <BuyStock /> : null}
+        {this.state.StockCalCulator ? <StockCalCulator /> : null}
+      </div>
+    );
+  }
+}
+
+export default StockApp;
