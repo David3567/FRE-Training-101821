ae64cd140d5fb6ace473fa9a6ed37062e814aebd
lh2510 <lh2510@nyu.edu>
2021-11-29 11:25:50 -0500

    update

--- redux/my-app/src/components/hoc/withBuyStock.js
+++ redux/my-app/src/components/hoc/withBuyStock.js
@@ -3,18 +3,13 @@ import store from '../../redux/store'
 
 export const withBuyStockData = (WrappedComponent) => {
 	return function NewComponent() {
-		const [value, setValue] = useState(0)
-		useEffect(() => {
-			store.subscribe(() => {
-				setValue((value) => value + 1)
-			})
-		}, [])
 		const handleAdd = () => {
 			store.dispatch({ type: 'increment', data: +1 })
 		}
 		const handleSub = () => {
 			// setCount(count - 1 < 0 ? 0 : count - 1)
-			store.dispatch({ type: 'decrement', data: +1 })
+			const count = store.getState()
+			if (count > 1) store.dispatch({ type: 'decrement', data: +1 })
 		}
 
 		return <WrappedComponent stockNum={store.getState()} handleAdd={handleAdd} handleSub={handleSub}></WrappedComponent>
--- redux/my-app/src/redux/store.js
+++ redux/my-app/src/redux/store.js
@@ -2,4 +2,3 @@ import { createStore } from 'redux' //createStore is used to create store object
 import reducer from './reducer'
 
 export default createStore(reducer)
-
