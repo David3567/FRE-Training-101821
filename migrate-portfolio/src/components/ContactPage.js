c4dfe5d3b113d1a4d1d899231cc297fa2a1abaf4
David An <taean2084@gmail.com>
2021-12-04 23:22:07 -0500

    portfolio migrate

---
+++ migrate-portfolio/src/components/ContactPage.js
@@ -0,0 +1,17 @@
+import Contact from "./contact components/contact";
+import Navigation from "./nav footer components/Navigation";
+import Footer from "./nav footer components/Footer";
+
+
+const ContactPage = () => {
+
+    return(
+        <>
+            <Navigation />
+            <Contact />
+            <Footer />
+        </>
+    );
+}
+
+export default ContactPage;
---
+++ migrate-portfolio/src/components/PortfolioPage.js
@@ -0,0 +1,17 @@
+import Navigation from './nav footer components/Navigation';
+import Projects from './portfolio components/Projects';
+import Footer from './nav footer components/Footer';
+
+const PortfolioPage = () => {
+
+    return(
+        <>
+            <Navigation />
+            <Projects />
+            <Footer />
+        </>
+        
+    );
+}
+
+export default PortfolioPage;
